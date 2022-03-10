d7ab27d423c1ba0d33429c10e31dcb238a95aee6
simonaa99 <simona.baosic@gmail.com>
2022-03-10 23:26:44 +0100

    Dodata implementacija metoda u okviru master grane

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,21 +1,40 @@
 package magacin;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import magacininterfejs.MagacinInterfejs;
 
 public class Magacin implements MagacinInterfejs {
 
+	private List<Artikal> artikli = new LinkedList<>();
+	
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		
+		for(Artikal a:artikli) {
+			if(a.getSifra() == artikal.getSifra()) {
+				a.setKolicina(a.getKolicina()+1);
+				break;
+			}
+			else {
+				artikli.add(artikal);
+				break;
+			}
+		}
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		
+		artikli.remove(artikal);
 	}
 
 	@Override
 	public Artikal vratiArtikal(int sifra) {
+		for(Artikal a:artikli) {
+			if(a.getSifra() == sifra) {
+			return a;
+			}
+		}
 		return null;
 	}
 
