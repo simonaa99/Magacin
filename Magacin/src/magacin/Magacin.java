551a804ffe979ad210846ce1b6f4ff79ef2431e5
simonaa99 <simona.baosic@gmail.com>
2022-03-10 22:57:07 +0100

    Kreirana klasa Magacin koja implementira interfejs MagacinInterfejs

---
+++ Magacin/src/magacin/Magacin.java
@@ -0,0 +1,22 @@
+package magacin;
+
+import magacininterfejs.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		
+	}
+
+	@Override
+	public Artikal vratiArtikal(int sifra) {
+		return null;
+	}
+
+}
