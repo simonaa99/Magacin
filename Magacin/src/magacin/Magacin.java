e676ffc5357adf2248ab6e4d4b3fdbb28d149570
simonaa99 <simona.baosic@gmail.com>
2022-03-10 23:02:32 +0100

    Implementirane metode u klasi Magacin

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,22 +1,31 @@
 package magacin;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import magacininterfejs.MagacinInterfejs;
 
 public class Magacin implements MagacinInterfejs {
 
+	private List<Artikal> artikli = new LinkedList<>();
+	
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		
+		artikli.add(artikal);
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		
+		artikli.remove(artikal);
 	}
 
 	@Override
 	public Artikal vratiArtikal(int sifra) {
-		return null;
+		for(Artikal a:artikli) {
+			if(a.getSifra() == sifra) {
+				return a;
+			}
+		}return null;
 	}
 
 }
